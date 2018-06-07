e1f2b3921ca01445451fc35507761564fc102374
jnsjns <spl4sh@live.se>
2018-06-07 15:05:25 +0200

    added tests both with mocking and without

---
+++ src/test/java/se/kth/hogk/poe_chat_parser/hello/GreetingControllerIT.java
@@ -0,0 +1,58 @@
+package se.kth.hogk.poe_chat_parser.hello;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import java.net.URL;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class GreetingControllerIT {
+
+    @LocalServerPort
+    private int port;
+
+    private URL base;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Before
+    public void setUp() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/");
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        ResponseEntity<String> response = template.getForEntity(base.toString(),
+                String.class);
+        assertThat(response.getBody(), equalTo("Welcome to my program"));
+    }
+
+    @Test
+    public void getGreeting() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/greeting");
+        ResponseEntity<String> response = template.getForEntity(base.toString(),
+                String.class);
+        assertThat(response.getBody(), equalTo("{\"id\":1,\"content\":\"Hello, World!\"}"));
+    }
+
+    @Test
+    public void getGreetingUser() throws Exception {
+        this.base = new URL("http://localhost:" + port + "/greeting?name=User");
+        ResponseEntity<String> response = template.getForEntity(base.toString(),
+                String.class);
+        assertThat(response.getBody(), equalTo("{\"id\":1,\"content\":\"Hello, User!\"}"));
+    }
+
+}
